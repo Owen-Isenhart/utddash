eb8aa85b412285dfad11f47042d1aa14537bf7f9
Jalen Abdi <jalenajj19@gmail.com>
2026-04-02 18:22:15 -0500

    Implemented JWT auth with protected user route

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -4,10 +4,14 @@ from passlib.context import CryptContext
 from fastapi import APIRouter, HTTPException
 from app.schemas import UserRegister, UserLogin, Token
 from app.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRATION
-
-print("auth.py loaded")
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends
 
 router = APIRouter()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 fake_users_db = {}
@@ -33,11 +37,35 @@ def register(user: UserRegister):
     return {"message": "User registered successfully"}
 
 @router.post("/login", response_model=Token)
-def login(user: UserLogin):
-    db_user = fake_users_db.get(user.email)
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    db_user = fake_users_db.get(form_data.username)
     if not db_user:
         raise HTTPException(status_code=401, detail="Invalid email or password")
-    if not verify_password(user.password, db_user["hashed_password"]):
+
+    if not verify_password(form_data.password, db_user["hashed_password"]):
         raise HTTPException(status_code=401, detail="Invalid email or password")
-    access_token = create_access_token(data={"sub": user.email})
+
+    access_token = create_access_token(data={"sub": form_data.username})
     return {"access_token": access_token, "token_type": "bearer"}
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = fake_users_db.get(email)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
+
+@router.get("/users/me")
+def read_users_me(current_user: dict = Depends(get_current_user)):
+    return {
+        "email": current_user["email"],
+        "role": current_user["role"]
+    }
