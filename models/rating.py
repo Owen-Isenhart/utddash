17870ba6684b8a6bc064ddd27ed39acd237de0a2
Safa Ahmed <sssafaa.432@gmail.com>
2026-04-08 21:12:01 -0500

    created ratings model

---
+++ models/rating.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, Text, Float, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from app.database import Base
+from datetime import datetime
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
+    rater_id = Column(Integer, ForeignKey("users.id"), nullable=False) # The user who gives the rating
+    ratee_id = Column(Integer, ForeignKey("users.id"), nullable=False) # The user who receives the rating
+
+    score = Column(Float, nullable=False) # e.g. 1.0 to 5.0
+    review_text = Column(Text, nullable=True)
+
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+
+    order = relationship("Order", back_populates="ratings")
