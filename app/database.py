73341928cd96a8b9239afa3a8f3305e9fc871df1
Safa Ahmed <sssafaa.432@gmail.com>
2026-04-15 22:09:00 -0500

    added get_db dependency to database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from backend.app.config import DATABASE_URL
 
-DATABASE_URL = "postgresql://postgres:utddash123@localhost:5432/utddash"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
