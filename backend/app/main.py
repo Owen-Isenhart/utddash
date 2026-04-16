00b2e31c979e0bbccad40619d2b8324c60e77626
Sanjana <vsanjana7871@gmail.com>
2026-04-16 18:22:26 -0500

    Edited backend structure with models and schema

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from backend.app.auth import router as auth_router
+<<<<<<< HEAD
+=======
+from backend.app.database import Base, engine
+from backend.app.models import user, order, rating
+
+Base.metadata.create_all(bind=engine)
+>>>>>>> 6f8a4b6 (Edited backend structure with models and schema)
 
 app = FastAPI(title="UTDDash API")
 
